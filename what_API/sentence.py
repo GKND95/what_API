5db17a16bbf198bc2c9c2c48224ee2177d857747
Gavin Dove <gdove@blaisetransit.com>
2020-01-25 13:07:32 -0500

    added sentence tokenization and header reduction logic'

--- what_API/sentence.py
+++ what_API/sentence.py
@@ -1,6 +1,6 @@
 import falcon
 import json
-import nltk
+from nltk.tokenize import sent_tokenize
 from google.cloud import translate_v2 as translate
 
 
@@ -8,13 +8,17 @@ class Resource(object):
 
     def on_post(self, req, resp):
 
+        # read input json to large string
         json_data = json.loads(req.stream.read())
         main_text = json_data["text"]
-        print(main_text)
 
-        sent_tokenized = nltk.sent_tokenize(main_text)
-        print(sent_tokenized)
+        # tokenize by sentence - outputs list of sentence strings
+        sent_tokenized = sent_tokenize(main_text)
 
+        # remove list elements with fewer than 15 characters
+        reduced_sent_tokenized = [i for i in sent_tokenized if len(i) > 15]
+
+        # Google translate stuff
         text = "Successful Test!"
         target = 'fr'
         model = 'nmt'
