00e9d11099db2f6790c8ee47540b0517e5305ba4
Gavin Dove <gdove@blaisetransit.com>
2020-01-25 12:19:57 -0500

    added punkt nltk data package

--- what_API/sentence.py
+++ what_API/sentence.py
@@ -1,4 +1,6 @@
 import falcon
+import json
+import nltk
 from google.cloud import translate_v2 as translate
 
 
@@ -6,13 +8,20 @@ class Resource(object):
 
     def on_post(self, req, resp):
 
+        json_data = json.loads(req.stream.read())
+        main_text = json_data["text"]
+        print(main_text)
+
+        sent_tokenized = nltk.sent_tokenize(main_text)
+        print(sent_tokenized)
+
         text = "Successful Test!"
         target = 'fr'
         model = 'nmt'
 
-        result = translate_client.translate(text, target_language=target, model=model)
+        #result = translate_client.translate(text, target_language=target, model=model)
 
-        resp.body = result['translatedText']
+        #resp.body = result['translatedText']
         resp.status = falcon.HTTP_200
 
 translate_client = translate.Client()
